5dc76acc68af365a8230ec8242707082eba743a0
programming-kid <programmingkid@live.com>
2013-05-28 03:08:26 +0530

    initial commit

---
+++ app/views/hello.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.default');
+
+@section('content')
+<h1>Hello  <a href="http://laravel.com/">Laravel 4</a> + <a href="http://twitter.github.io/bootstrap/">Bootstrap 2.3.2.</a></h1>
+@stop
